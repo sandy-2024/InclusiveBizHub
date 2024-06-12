ffae3d3971f20a0a9fc68d57e6f9f5630d4b85c3
sandy-2024 <sandesh3740@gmail.com>
2024-06-12 18:57:42 +0545

    updated home and logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,16 @@ import { Link } from "react-router-dom";
 
 
 const Home = () => {
+
+
+const jumbotron = {
+  backgroundColor: '#f4511e',
+  color: '#fff',
+  padding: '100px 25px'
+};
+
+
+
   const buttonStyle = {
     margin: '0 10px', // Add some space between buttons
     padding: '10px 20px',
@@ -45,7 +55,7 @@ const Home = () => {
         <Link to="/SocialCauses" className="btn btn-secondary m-2">See Causes</Link>
       </div>
 
-      <div className="container text-center mt-5">
+      <div className="container text-center mt-5" style={{paddingBottom: "3rem"}}>
         <div
           style={{
             color: "black",
@@ -58,6 +68,26 @@ const Home = () => {
           </span>
         </div>
       </div>
+      <div className="jumbotron text-center" style={{ backgroundColor: 'orange', padding: '2rem' }}>
+      <h1 className="display-4">Get Updated!</h1>
+      <p >We specialize in cause solution.</p>
+      <form className="form-inline justify-content-center" style={{ maxWidth: '400px', margin: '0 auto' }}>
+        <div className="input-group" style={{ width: '100%' }}>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email Address"
+            required
+            style={{ flex: 1 }}
+          />
+          <div className="input-group-append">
+            <button type="button" className="btn btn-danger">
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
     </>
   );
 };
--- src/components/SocialCauses.jsx
+++ src/components/SocialCauses.jsx
@@ -1,12 +1,63 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Registration from './Registration';
 
 const SocialCauses = () => {
   return (
-    <>
-    
-    </>
+    <div className="container pt-5">
+      <div className="row">
+        <div className="col-md-4 mb-4">
+          <div className="card" style={{ width: '18rem' }}>
+            <img src="https://cdn.shopify.com/s/files/1/0564/9321/1807/articles/1._anh_bia_2b83c021-34fe-465e-8a3c-520e5c73e9c5.jpg?v=1701595625" className="card-img-top" alt="Environment Protection" />
+            <div className="card-body">
+              <h5 className="card-title">Environment Protection</h5>
+              <p className="card-text">Environmental protection is the practice of protecting the natural environment by individuals, groups and governments.</p>
+              <Link to="/login" className="btn btn-primary">
+                Learn more
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4 mb-4">
+          <div className="card" style={{ width: '18rem' }}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV_YemtxMCpa19VRnqKlwrnXE2yYgKVIXakg&s" className="card-img-top" alt="Education for all" />
+            <div className="card-body">
+              <h5 className="card-title">Education for all</h5>
+              <p className="card-text">Education is the basic building block of every society.</p>
+              <Link to="/login" className="btn btn-primary">
+                Learn more
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4 mb-4">
+          <div className="card" style={{ width: '18rem' }}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5yrFawHqcdKpYscC8WHPG4lTTvgoHNL5z8g&s" className="card-img-top" alt="Healthcare Improvement" />
+            <div className="card-body">
+              <h5 className="card-title">Healthcare Improvement</h5>
+              <p className="card-text">Quality improvement is the framework used to systematically improve care.</p>
+              <Link to="/login" className="btn btn-primary">
+                Learn more
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4 mb-4">
+          <div className="card" style={{ width: '18rem' }}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ8uPZyfjmgKWgqFMjAjP8esSMRWsN_juYtA&s" className="card-img-top" alt="Poverty alleviation" />
+            <div className="card-body">
+              <h5 className="card-title">Poverty alleviation</h5>
+              <p className="card-text">Poverty reduction, poverty relief, or poverty alleviation is a set of measures, both economic and humanitarian, that are intended to permanently lift people out of poverty.</p>
+              <Link to="/login" className="btn btn-primary">
+                Learn more
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
